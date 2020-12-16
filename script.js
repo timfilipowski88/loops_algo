5dde732567cec7bc52219f370e252dea7d4a8a54
tfilipow7 <43181506+tfilipow7@users.noreply.github.com>
2020-12-16 10:10:43 -0700

    Complete

---
+++ script.js
@@ -0,0 +1,12 @@
+for (var i = 1; i < 20; i++) {
+    if (i % 2 != 0) {
+        console.log(i);
+    }
+}
+
+
+var sum = 0;
+for (var num = 1; num < 6; num++) {
+    sum = sum + num;
+    console.log(num, sum);
+}
